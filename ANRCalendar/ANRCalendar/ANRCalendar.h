d6148a3135a9ac9f2cc3a644113a3cffab7fb797
ANR Tech Solutions <info@anrtechsolutions.com>
2016-03-12 15:57:19 +0000

    Project setup.
    Added gitignore for AppCode, Xcode and added framework project.

---
+++ ANRCalendar/ANRCalendar/ANRCalendar.h
@@ -0,0 +1,19 @@
+//
+//  ANRCalendar.h
+//  ANRCalendar
+//
+//  Created by Jamil Nawaz on 12/03/2016.
+//  Copyright © 2016 ANRTechSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ANRCalendar.
+FOUNDATION_EXPORT double ANRCalendarVersionNumber;
+
+//! Project version string for ANRCalendar.
+FOUNDATION_EXPORT const unsigned char ANRCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANRCalendar/PublicHeader.h>
+
+
